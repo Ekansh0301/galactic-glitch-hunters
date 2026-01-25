34754bb8728ddf87268e5f4a622d11e544a98af9
Aman Srivastava <amansrivastava80815@gmail.com>
2026-01-25 20:24:36 +0530

    ButtonCommit
    buttons are working

--- Game/Scripts/CharacterCreation.gd
+++ Game/Scripts/CharacterCreation.gd
@@ -1,8 +1,11 @@
 extends Control
 
-func _on_male_pressed(): GameManager.selected_gender_id = 1
-func _on_female_pressed(): GameManager.selected_gender_id = 2
-func _on_nb_pressed(): GameManager.selected_gender_id = 3
+var current_gender = 1
+
+func _on_male_pressed(): current_gender = 1
+func _on_female_pressed(): current_gender = 2
+func _on_other_pressed(): current_gender = 3
 
 func _on_confirm_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Hub.tscn")
+	GameManager.save_player_selection(GameManager.player_name, current_gender)
+	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
